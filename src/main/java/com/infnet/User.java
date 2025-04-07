dce14682754151e3e8aa6ac6e08bb01e3ad75f84
jvcmtr <jvictorcmtr@gmail.com>
2025-04-06 22:48:02 -0300

    Refactored user class

--- src/main/java/com/infnet/User.java
+++ src/main/java/com/infnet/User.java
@@ -1,35 +1,65 @@
 package com.infnet;
 
-import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class User{
-    private String nome;
-    private String email;
-    private ECargo cargo;
+    private final int id;
+    private final String nome;
+    private final String email;
+    private final ECargo cargo;
+    private final int idade;
+    private final BigDecimal salario;
 
-    public boolean atualizarEmail(String email){
-        if(!email.contains("@")){
-            return false;
-        }
+    public User(int id, String nome, String email, ECargo cargo, int idade, BigDecimal salario){
+        validateEmail(email);
+        validateIdade(idade);
+        validateSalario(salario);
 
-        if(email.contains(" ")){
-            return false;
-        }
-
+        this.id = id;
+        this.nome = nome;
         this.email = email;
-        return true;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public User atualizarEmail(String email){
+        validateEmail(email);
+        return new User(id, email, email, cargo, idade, salario);
     }   
 
-    private void definirCargo(ECargo cargo){
-        this.cargo = cargo;
+    private User definirCargo(ECargo cargo){
+        return new User(id, email, email, cargo, idade, salario);
     }
 
     private void exibirDetalhes(){
-        System.err.println(nome);
+        System.out.println(nome);
         System.out.println("\tE-mail : " + email + "\n\tCargo : " + cargo);
     }
 
+    private static void validateEmail(String email){
+
+        if(!email.contains("@")){
+            throw new IllegalArgumentException("O email do usuario deve conter '@' ");
+        }
+
+        if(email.contains(" ") || email.contains("\t")){
+            throw new IllegalArgumentException("O email do usuario não pode conter espaços em branco");
+        }
+    }
+
+    private static void validateSalario(BigDecimal salario){
+        if(salario.compareTo( BigDecimal.ZERO ) != 1){
+            throw new IllegalArgumentException("O usuario deve ter um salario maior do que zero ");
+        }
+    }
+
+    private static void validateIdade(int idade){
+        if(idade < 18 ){
+            throw new IllegalArgumentException("O usuario deve ser maior de idade");
+        }
+    }
 }
